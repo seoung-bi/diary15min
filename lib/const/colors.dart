adb3223c0b3b34016f1f1a092444d1b12ba8b69b
Seung-bi <elxoevely@naver.com>
2024-12-04 18:13:37 +0900

    Add files via upload

---
+++ lib/const/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const DARK_PINK_COLOR = Color(0xFFFFCDE9); //카드박스
+const WHITE_COLOR = Color(0xFFFBF7FD);
+const LIGHT_YELLOW_COLOR = Color(0xFFF9F0EB); //배경
+const YELLOW_COLOR = Color(0xFFFFF897);
+const PRIMARY_COLOR = Color(0xFF000200); //기본 글씨
+const BLUE_COLOR = Color(0xFFA1C9FD); //상단바
+
+
+
+
+
+
+
+
